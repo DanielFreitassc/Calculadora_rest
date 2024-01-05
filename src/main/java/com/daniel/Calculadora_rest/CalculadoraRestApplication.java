979734b274645f846c1a1f1bbf21e8228ef41169
danielfreitassc <danielfreitassc@gmail.com>
2024-01-05 19:43:28 -0300

    first commit

---
+++ src/main/java/com/daniel/Calculadora_rest/CalculadoraRestApplication.java
@@ -0,0 +1,13 @@
+package com.daniel.Calculadora_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculadoraRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculadoraRestApplication.class, args);
+	}
+
+}
